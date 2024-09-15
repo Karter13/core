1ea8b9dc034496208d1e8dbda4f037c8551a3af3
Карамзин Михаил <mikhail.karamzin@netprint.ru>
2024-09-15 22:04:18 +0300

    feature: add github authorization

--- src/app/_providers/app-provider.tsx
+++ src/app/_providers/app-provider.tsx
@@ -3,11 +3,13 @@
 import React from "react";
 import { ThemeProvider } from "@/features/theme/theme-provider";
 import { ComposeChildren } from "@/shared/lib/react";
+import { AppSessionProvider } from "@/entities/session/app-session-provider";
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
   return (
     <ComposeChildren>
       <ThemeProvider />
+      <AppSessionProvider/>
       {children}
     </ComposeChildren>
   );
